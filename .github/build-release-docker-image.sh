a723485d9de2503f5c201572e71cfacb47bb03ab
vvukomanTT <vvukoman@tenstorrent.com>
2026-04-21 05:35:03 +0000

    initial version

---
+++ .github/build-release-docker-image.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+if [ $# -ne 2 ]; then
+    echo "Error: Exactly 2 arguments are required."
+    echo "Usage: $0 <project> <docker-commit-tag>"
+    exit 1
+fi
+
+PROJECT=$1
+COMMIT_TAG=$2
+
+IMAGE_NAME=ghcr.io/tenstorrent/$PROJECT-slim
+
+echo "Building image $IMAGE_NAME:$COMMIT_TAG"
+docker build \
+  --progress=plain \
+  -t $IMAGE_NAME:$COMMIT_TAG \
+  -f .github/Dockerfile.release-ubuntu .
+
+echo "Pushing image $IMAGE_NAME:$COMMIT_TAG"
+docker push $IMAGE_NAME:$COMMIT_TAG
+
+echo "Image built and pushed successfully"
+echo $IMAGE_NAME
+echo $IMAGE_NAME:$COMMIT_TAG
